88e79476c5bf1c77d8c2bf6ab33508b3a1d664ff
TiendaCompu <TIENDACOMPU@GMAIL.COM>
2026-07-23 04:27:51 +0200

    Add ZYTRONIX mobile app configuration

---
+++ android-wrapper/app/src/main/java/online/zytronix/mobile/AppConfig.java
@@ -0,0 +1,7 @@
+package online.zytronix.mobile;
+
+public final class AppConfig {
+    public static final String START_URL = "https://zytronix.online/csatrieste/mobile/";
+    public static final String ALLOWED_HOST = "zytronix.online";
+    private AppConfig() {}
+}
